bed7a40c1163e4c4009ccb4d23916e6380d8682d
Linus Plume <plumee_91@hotmail.com>
2018-05-18 09:05:01 +0200

    Small fix

--- tostudents/src/app/task.service.ts
+++ tostudents/src/app/task.service.ts
@@ -28,20 +28,19 @@ export class TaskService {
     return new Observable((observer) => {
       this.observer = observer;
       observer.next(this.taskList)
-    })
+    });
     // return an observable of a task array, filtered by the passed in status...
   }
 
   updateTask(id: number, status: StatusType) {
     // complete the code to update a task's status...
     this.taskList.forEach((task) => {
-      if(task.id === id) {
+      if (task.id === id) {
         task.status = status;
       }
     });
     this.observer.next(this.taskList);
   }
-
 
   addTask(title: string, description: string) {
     this.taskList.push({
